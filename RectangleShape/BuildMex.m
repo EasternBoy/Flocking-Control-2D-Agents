f673c1d3d87f2ae029a3f2fbf2eeb32b367d4842
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-04-28 01:57:58 -0500

    Update BuildMex.m

--- RectangleShape/BuildMex.m
+++ RectangleShape/BuildMex.m
@@ -1,1 +1,13 @@
+qi = [1;1;0];
+qj = [2;2;0];
+qod = [0;0;0];
+poi = [0.4 0; 0 -0.2; -0.4 0; 0 0.2]';
+poj = [0.4 0; 0 -0.2; -0.4 0; 0 0.2]';
+Mi = 4;
+Mj = 4;
+dD = 1; 
+dM = 5;
+lam = 0.1;
+mu = 100;
 
+codegen -report GH_ij.m -args {qi,qj,qod,poi,poj,Mi,Mj,dD,dM,lam,mu}
